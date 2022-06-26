f92137ed0fa9ef01554b83a5b03d7a661594a9e3
Stepan Rabotkin <epicstyt@gmail.com>
2022-06-26 23:59:13 +0300

    feat: add guild test

---
+++ vimeworld/guild_test.go
@@ -0,0 +1,39 @@
+package vimeworld_test
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestClient_GuildSearch(t *testing.T) {
+	t.Parallel()
+
+	guilds, err := client.GuildSearch(context.Background(), "f5")
+	require.NoError(t, err)
+	require.NotEmpty(t, guilds)
+}
+
+func TestClient_GetGuildByID(t *testing.T) {
+	t.Parallel()
+
+	guild, err := client.GetGuildByID(context.Background(), 1)
+	require.NoError(t, err)
+	require.Equal(t, "Fantastic Five", guild.Name)
+}
+
+func TestClient_GetGuildByName(t *testing.T) {
+	t.Parallel()
+
+	guild, err := client.GetGuildByName(context.Background(), "Fantastic Five")
+	require.NoError(t, err)
+	require.Equal(t, 1, guild.ID)
+}
+
+func TestClient_GetGuildByTag(t *testing.T) {
+	t.Parallel()
+
+	guild, err := client.GetGuildByTag(context.Background(), "-F5-")
+	require.NoError(t, err)
+	require.Equal(t, "Fantastic Five", guild.Name)
+}
