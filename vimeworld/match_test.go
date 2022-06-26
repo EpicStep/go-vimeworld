2588e789a9878a1b9292fb37b9bc9bbdc43cdb07
Stepan Rabotkin <epicstyt@gmail.com>
2022-06-27 01:48:01 +0300

    feat: add match tests

---
+++ vimeworld/match_test.go
@@ -0,0 +1,31 @@
+package vimeworld_test
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestClient_GetMatchLatest(t *testing.T) {
+	t.Parallel()
+
+	m, err := client.GetMatchLatest(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, m)
+}
+
+func TestClient_GetMatchListBefore(t *testing.T) {
+	t.Parallel()
+
+	before, err := client.GetMatchListBefore(context.Background(), "461280233589309440", 1)
+	require.NoError(t, err)
+	require.Equal(t, "BP", before[0].Game)
+}
+
+func TestClient_GetMatchListAfter(t *testing.T) {
+	t.Parallel()
+
+	before, err := client.GetMatchListAfter(context.Background(), "461280233589309440", 1)
+	require.NoError(t, err)
+	require.Equal(t, "BWH", before[0].Game)
+}
